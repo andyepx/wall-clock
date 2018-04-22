e7d77f21c92493845f2f626ac620b1f3b22a4d4b
Andrea Epifani <andrea@andreaepifani.com>
2018-04-22 19:07:03 +1000

    First

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {Insomnia} from '@ionic-native/insomnia';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private currentTime: string = "";
+  private currentDate: string = "";
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+              private insomnia: Insomnia) {
+    this.currentTime = this.getTime();
+    this.currentDate = this.getDate();
+  }
 
+  ngOnInit() {
+    this.insomnia.keepAwake()
+      .then(() => {
+        setTimeout(() => {
+          this.currentTime = this.getTime();
+          this.currentDate = this.getDate();
+        }, 1000 * 60)
+      }, () => {
+
+      })
+  }
+
+  private getTime(): string {
+    const x = new Date();
+    return x.getHours() + ":" + x.getMinutes();
+  }
+
+  private getDate(): string {
+    const x = new Date();
+    return x.getDate() + "/" + (x.getMonth() + 1) + "/" + x.getFullYear();
   }
 
 }
