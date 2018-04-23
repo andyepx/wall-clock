c8b520c3adca4679eb7e691da8c9ad14591cb57b
Andrea Epifani <andrea@andreaepifani.com>
2018-04-24 07:09:25 +1000

    More updates

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -18,25 +18,29 @@ export class HomePage {
   }
 
   ngOnInit() {
-    this.insomnia.keepAwake()
-      .then(() => {
-        setTimeout(() => {
+    setInterval(() => {
+      this.insomnia.keepAwake()
+        .then(() => {
           this.currentTime = this.getTime();
           this.currentDate = this.getDate();
-        }, 1000 * 60)
-      }, () => {
+        }, () => {
 
-      })
+        })
+    }, 1000 * 60)
   }
 
   private getTime(): string {
     const x = new Date();
-    return x.getHours() + ":" + x.getMinutes();
+    const h = x.getHours() < 10 ? `0${x.getHours()}` : x.getHours();
+    const m = x.getMinutes() < 10 ? `0${x.getMinutes()}` : x.getMinutes();
+    return `${h}:${m}`;
   }
 
   private getDate(): string {
     const x = new Date();
-    return x.getDate() + "/" + (x.getMonth() + 1) + "/" + x.getFullYear();
+    const d = x.getDate() < 10 ? `0${x.getDate()}` : x.getDate();
+    const m = (x.getMonth() + 1) < 10 ? `0${(x.getMonth() + 1)}` : (x.getMonth() + 1);
+    return `${d}/${m}/${x.getFullYear()}`;
   }
 
 }
